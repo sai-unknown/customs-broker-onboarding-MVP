4372328ec4e82f89c0857963e0b94b5828492425
SAi <sandeeprathod455161@gmail.com>
2026-07-28 01:44:29 +0530

    feat(auth): implement broker registration with bcrypt, zod and JWT

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -2,26 +2,19 @@ import { registerSchema } from "../validators/authValidator.js";
 import { registerBroker } from "../services/authService.js";
 import { generateToken } from "../utils/jwt.js";
 
-export const register = async (req, res, next) => {
-  try {
-    console.log("Content-Type:", req.headers["content-type"]);
-    console.log("Body:", req.body);
+export const register = asyncHandler(async (req, res) => {
+  const validatedData = registerSchema.parse(req.body);
 
-    const validatedData = registerSchema.parse(req.body);
+  const broker = await registerBroker(validatedData);
 
-    const broker = await registerBroker(validatedData);
+  const token = generateToken(broker.id);
 
-    const token = generateToken(broker.id);
-
-    res.status(201).json({
-      success: true,
-      message: "Registration successful",
-      data: {
-        user: broker,
-        token,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  res.status(201).json({
+    success: true,
+    message: "Registration successful",
+    data: {
+      user: broker,
+      token,
+    },
+  });
+});
